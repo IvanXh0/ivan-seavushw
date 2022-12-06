d4eb47bf579a262f0af0ae1a580d045bdd3bf52a
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2022-12-06 22:05:28 +0100

    JavaScript-Homework-1

---
+++ JS-Homework-1-Ivan/script.js
@@ -0,0 +1,8 @@
+const phones = 30;
+const priceOfOnePhone = 119.95;
+const taxRate = 1.05;
+
+let totalPrice = (priceOfOnePhone * taxRate) * phones;
+
+console.log(`The total cost is $${totalPrice}.`)
+alert(`The total cost is $${totalPrice}.`);
